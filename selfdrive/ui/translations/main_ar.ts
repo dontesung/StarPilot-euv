258f45c5561d08b0c9d4f8de008b3c86e8d24df5
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-08-22 13:05:40 -0500

    Revert "Updates"
    This reverts commit f2fcd52e6ee0070271247148fe11b8a0229dbfd5.

--- selfdrive/ui/translations/main_ar.ts
+++ selfdrive/ui/translations/main_ar.ts
@@ -4211,10 +4211,6 @@ Want to submit your own animations? Share them in the "custom-themes" channel on
         <source>The width of the driving path on the driving screen.&lt;br&gt;&lt;br&gt;Default &lt;b&gt;(1.9 meters)&lt;/b&gt; matches the width of a &lt;b&gt;2019 Lexus ES 350&lt;/b&gt;.</source>
         <translation type="gpt-4o-generated">عرض مسار القيادة على شاشة القيادة.&lt;br&gt;&lt;br&gt;الافتراضي &lt;b&gt;(1.9 متر)&lt;/b&gt; يتطابق مع عرض &lt;b&gt;لكزس ES 350 2019&lt;/b&gt;.</translation>
     </message>
-    <message>
-        <source>Driving Model: Current</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>FrogPilotWheelPanel</name>
--- selfdrive/ui/translations/main_fr.ts
+++ selfdrive/ui/translations/main_fr.ts
@@ -4209,10 +4209,6 @@ Want to submit your own animations? Share them in the "custom-themes" channel on
         <source>The width of the driving path on the driving screen.&lt;br&gt;&lt;br&gt;Default &lt;b&gt;(1.9 meters)&lt;/b&gt; matches the width of a &lt;b&gt;2019 Lexus ES 350&lt;/b&gt;.</source>
         <translation type="gpt-4o-generated">La largeur du chemin de conduite sur l'écran de conduite.&lt;br&gt;&lt;br&gt;Par défaut &lt;b&gt;(1,9 mètres)&lt;/b&gt; correspond à la largeur d'une &lt;b&gt;Lexus ES 350 de 2019&lt;/b&gt;.</translation>
     </message>
-    <message>
-        <source>Driving Model: Current</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>FrogPilotWheelPanel</name>
